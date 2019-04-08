6be092487aecd0819362f9ca9ab426804c1e4795
Leanne Cheung <leannecheung@Leannes-MacBook-Air.local>
2019-04-07 20:29:16 -0600

    added more images to the howto

--- src/components/HowTo/HowTo.jsx
+++ src/components/HowTo/HowTo.jsx
@@ -4,6 +4,8 @@ import step2 from './step2.png'
 import step3p0 from './step3.0.png'
 import step3p1 from './step3.1.png'
 import step4 from './step4.png'
+import info from './info.png'
+import dot from './dot.png'
 
 import './HowTo.css'
 
@@ -17,29 +19,33 @@ export default function HowTo() {
         <br></br>
         <br></br>
 
-        <h4>2. Send some Zaps into your MetaMask wallet. See the right side of this screen-shot for the amount of Zaps this demo wallet has.<br/>
-        On the right side of the page there’s the information for the signal. There’s also the input field for users to put in number to determine how many Zaps they need to get a Dot. For instance, Trend Signals requires 0.01 Zap for 1 dot.</h4>
+        <h4>2. Send some Zaps into your MetaMask wallet. See the right side of this screen-shot for the amount of Zaps this demo wallet has.</h4>
         <img src={step2} alt='step 2' />
 
         <br></br>
+        <h4>On the right side of the page there’s the information for the signal. There’s also the input field for users to put in number to determine how many Zaps they need to get a Dot. For instance, Trend Signals requires 0.01 Zap for 1 dot.</h4>
+        <img src={info} alt='info' />
+
+        <br></br>
         <br></br>
         <br></br>
 
-        <h4>3. Approve some Zaps. <br/>
-        Fill out the number of Zaps you want to approve. Then click on the 'Approve' buton. It'll pull up MetaMask and you can click on 'Send' to send the request to the Ethereum contract.
+        <h4>3. Approve some Zaps by filling out the number of Zaps you want. Then click on the 'Approve' buton. It'll pull up MetaMask and you can click on 'Send' to send the request to the Ethereum contract.
         </h4>
         <img src={step3p0} alt='step 3.0' />
         <br></br>
         <br></br>
         <br></br>
 
-        <h4>4. Bond dots. </h4>
+        <h4>4. Bond dots. This step also calls the smart contract via MetaMask so that you can get the Dots, which is gives you access to subscribe to the blocks in the next step to get data.</h4>
+        <img src={dot} alt='dot' />
+
         <br></br>
         <br></br>
         <br></br>
 
         <h4>5. Subscribe to blocks. </h4>
-        <img src={step4} alt='step 4' />
+        <img src={step4} alt='step 5' />
         <br></br>
         <br></br>
         <br></br>
